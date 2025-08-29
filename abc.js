3d3990be588a31cd34a015688a976e5359cf763a
karim-lucky <karimccw8100@gmail.com>
2025-08-29 12:26:26 +0500

    study js basic

---
+++ abc.js
@@ -0,0 +1,8 @@
+
+
+
+let textInsideAbc=document.getElementById("textInsideAbc");
+
+document.getElementById("one").innerText="This is text added by abc.js file";
+
+console.log(textInsideAbc.innerText);
